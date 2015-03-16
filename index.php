086de0e6bc2df0f2a394fe4769830099e49b8c28
andreu <andreudekker@hotmail.com>
2015-03-16 15:00:10 -0500

    add index.php

---
+++ index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html class="html">
+ <head>
+
+  <script type="text/javascript">
+   if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required":["museredirect.js", "jquery-1.8.3.min.js", "museutils.js", "jquery.watch.js", "jquery.scrolleffects.js", "webpro.js", "musewpslideshow.js", "jquery.museoverlay.js", "touchswipe.js", "index.css"], "outOfDate":[]};
+</script>
+  
+  <script src="scripts/museredirect.js?3986430445" type="text/javascript"></script>
+  
+  <script type="text/javascript">
+   Muse.Redirect.redirect('desktop', '', 'phone/index.html', '');
+</script>
+  
+  <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+  <meta name="generator" content="2014.0.0.264"/>
+  <title>Inicio</title>
+  <link media="only screen and (max-device-width: 370px)" rel="alternate" href="http://www.indexstudio.co/phone/index.html"/>
+  <!-- CSS -->
+  <link rel="stylesheet" type="text/css" href="css/site_global.css?3991134328"/>
+  <link rel="stylesheet" type="text/css" href="css/master_a-p_g_-maestra.css?50429864"/>
+  <link rel="stylesheet" type="text/css" href="css/index.css?297119235" id="pagesheet"/>
+  <!-- Other scripts -->
+  <script type="text/javascript">
+   document.documentElement.className += ' js';
+var __adobewebfontsappname__ = "muse";
+</script>
+  <!-- JS includes -->
+  <script type="text/javascript">
+   document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//webfonts.creativecloud.com/abel:n4:all;alfa-slab-one:n4:all;advent-pro:n7:all.js" type="text/javascript">\x3C/script>');
+</script>
+   </head>
+ <body>
+
+  <div class="clearfix" id="page"><!-- column -->
+   <div class="position_content" id="page_position_content">
+    <div class="browser_width colelem" id="u633-bw">
+     <div id="u633"><!-- group -->
+      <div class="clearfix" id="u633_align_to_page">
+       <a class="anchor_item grpelem" id="nosotros"></a>
+      </div>
+     </div>
+    </div>
+    <div class="clearfix colelem" id="pu510"><!-- group -->
+     <div class="browser_width" id="u510-bw">
+      <div class="shadow gradient" id="u510"><!-- simple frame --></div>
+     </div>
+     <div class="clearfix" id="u639-4"><!-- content -->
+      <p id="u639-2"><span class="textogrande" id="u639">indexstudio.co</span></p>
+     </div>
+     <div class="clearfix ose_pre_init mse_pre_init" id="u609-4"><!-- content -->
+      <p id="u609-2"><span class="textogrande" id="u609">indexstudio.co</span></p>
+     </div>
+     <a class="nonblock nontext anim_swing rounded-corners" id="u634" href="index.html#nosotros"><!-- simple frame --></a>
+     <a class="nonblock nontext anim_swing rounded-corners" id="u635" href="index.html#servicios"><!-- simple frame --></a>
+     <a class="nonblock nontext anim_swing rounded-corners" id="u636" href="index.html#contacto"><!-- simple frame --></a>
+    </div>
+    <div class="SlideShowWidget clearfix colelem" id="slideshowu511"><!-- none box -->
+     <div class="popup_anchor" id="u514popup">
+      <div class="SlideShowContentPanel clearfix" id="u514"><!-- stack box -->
+       <div class="SSSlide clip_frame grpelem" id="u517"><!-- image -->
+        <img class="ImageInclude" id="u517_img" data-src="images/04.jpg" src="images/blank.gif" alt="" data-width="1150" data-height="586"/>
+       </div>
+       <div class="SSSlide invi clip_frame grpelem" id="u574"><!-- image -->
+        <img class="ImageInclude" id="u574_img" data-src="images/03.jpg" src="images/blank.gif" alt="" data-width="1150" data-height="586"/>
+       </div>
+       <div class="SSSlide invi clip_frame grpelem" id="u623"><!-- image -->
+        <img class="ImageInclude" id="u623_img" data-src="images/01.jpg" src="images/blank.gif" alt="" data-width="1150" data-height="586"/>
+       </div>
+      </div>
+     </div>
+     <div class="popup_anchor" id="u521popup">
+      <div class="SlideShowCaptionPanel clearfix" id="u521"><!-- stack box -->
+       <div class="SSSlideCaption clearfix grpelem" id="u524-4"><!-- content -->
+        <p id="u524-2"><span class="Estilo-de-car-cter-1" id="u524">Somos un estudio que desarrolla y diseña experiencias digitales.</span></p>
+       </div>
+       <div class="SSSlideCaption invi clearfix grpelem" id="u580-4"><!-- content -->
+        <p id="u580-2"><span class="Estilo-de-car-cter-1" id="u580">Somos creativos, innovadores y amamos&nbsp; lo que hacemos.</span></p>
+       </div>
+       <div class="SSSlideCaption invi clearfix grpelem" id="u629-4"><!-- content -->
+        <p id="u629-2"><span class="Estilo-de-car-cter-1" id="u629">Nos apasionan las nuevas tecnologías.</span></p>
+       </div>
+      </div>
+     </div>
+     <div class="popup_anchor" id="u526-4popup">
+      <div class="SSPreviousButton clearfix" id="u526-4"><!-- content -->
+       <p id="u526-2"><span class="textogrande" id="u526">&lt;</span></p>
+      </div>
+     </div>
+     <div class="popup_anchor" id="u536-4popup">
+      <div class="SSNextButton clearfix" id="u536-4"><!-- content -->
+       <p id="u536-2"><span class="textogrande" id="u536">&gt;</span></p>
+      </div>
+     </div>
+    </div>
+    <div class="clearfix colelem" id="pu632"><!-- group -->
+     <div class="browser_width grpelem" id="u632-bw">
+      <div id="u632"><!-- simple frame --></div>
+     </div>
+     <a class="anchor_item grpelem" id="servicios"></a>
+     <div class="clearfix grpelem" id="u289-4"><!-- content -->
+      <p id="u289-2"><span class="textogrande" id="u289">Nosotros</span></p>
+     </div>
+    </div>
+    <div class="clearfix colelem" id="pu253"><!-- group -->
+     <div class="browser_width grpelem" id="u253-bw">
+      <div class="gradient" id="u253"><!-- simple frame --></div>
+     </div>
+     <div class="Estilo-de-p-rrafo clearfix grpelem" id="u77-4"><!-- content -->
+      <p id="u77-2"><span class="title" id="u77">Asesorias y Consultorias</span></p>
+     </div>
+     <div class="Estilo-de-p-rrafo clearfix grpelem" id="u79-8"><!-- content -->
+      <p id="u79-5"><span class="subtitle">Analizamos el contexto y profundizamos para darle sentido </span>a la solución final. <br/>Lo que se traduce en mejores experiencias.</p>
+      <p id="u79-6">&nbsp;</p>
+     </div>
+     <div class="Estilo-de-p-rrafo clearfix grpelem" id="u84-4"><!-- content -->
+      <p id="u84-2"><span class="title" id="u84">Diseño&nbsp; exclusivo</span></p>
+     </div>
+     <div class="Estilo-de-p-rrafo clearfix grpelem" id="u85-6"><!-- content -->
+      <p id="u85-3"><span class="subtitle">Hacemos propuestas que buscan exclusividad. Seleccionamos </span>un lenguaje visual de alta calidad que comunique lo simple, lo estético y lo verdadero.</p>
+      <p id="u85-4">&nbsp;</p>
+     </div>
+     <div class="Estilo-de-p-rrafo clearfix grpelem" id="u86-4"><!-- content -->
+      <p id="u86-2"><span class="title" id="u86">Plataformas digitales</span></p>
+     </div>
+     <div class="Estilo-de-p-rrafo clearfix grpelem" id="u87-7"><!-- content -->
+      <p id="u87-2">Desarrollamos arquitecturas flexibles para resaltar la experiencia del usuario. Nuestras herramientas</p>
+      <p id="u87-4">&nbsp;las nuevas tecnologías.</p>
+      <p id="u87-5">&nbsp;</p>
+     </div>
+     <div class="clearfix grpelem" id="u256-4"><!-- content -->
+      <p id="u256-2"><span class="textogrande" id="u256">Servicios</span></p>
+     </div>
+     <a class="anchor_item grpelem" id="contacto"></a>
+    </div>
+    <div class="clearfix colelem" id="pu203"><!-- group -->
+     <div class="browser_width grpelem" id="u203-bw">
+      <div id="u203"><!-- group -->
+       <div class="clearfix" id="u203_align_to_page">
+        <div class="clearfix grpelem" id="u252-4"><!-- content -->
+         <p id="u252-2"><span class="textogrande" id="u252">Contactenos</span></p>
+        </div>
+        <div class="clearfix grpelem" id="u297-4"><!-- content -->
+         <p><span class="title" id="u297">Cuéntenos sus inquietudes, proyectos y propuestas.</span></p>
+        </div>
+       </div>
+      </div>
+     </div>
+     <form class="form-grp clearfix grpelem" id="widgetu234" method="post" enctype="multipart/form-data" action="scripts/form-u234.php"><!-- none box -->
+      <div class="fld-grp clearfix grpelem" id="widgetu242" data-required="true"><!-- none box -->
+       <label class="fld-label actAsDiv clearfix grpelem" id="u244-4" for="widgetu242_input"><!-- content -->
+        <span class="actAsPara"><span class="Sub_formulario" id="u244">Nombre:</span></span>
+       </label>
+       <span class="fld-input NoWrap actAsDiv clearfix grpelem" id="u245-3"><!-- content --><input class="wrapped-input" type="text" spellcheck="false" id="widgetu242_input" name="custom_U242" tabindex="1"/></span>
+      </div>
+      <div class="fld-grp clearfix grpelem" id="widgetu247" data-required="true" data-type="email"><!-- none box -->
+       <label class="fld-label actAsDiv clearfix grpelem" id="u249-4" for="widgetu247_input"><!-- content -->
+        <span class="actAsPara"><span class="Sub_formulario" id="u249">Correo electrónico</span></span>
+       </label>
+       <span class="fld-input NoWrap actAsDiv clearfix grpelem" id="u250-3"><!-- content --><input class="wrapped-input" type="text" spellcheck="false" id="widgetu247_input" name="Email" tabindex="2"/></span>
+      </div>
+      <div class="clearfix grpelem" id="u240-4"><!-- content -->
+       <p>Enviando formulario...</p>
+      </div>
+      <div class="clearfix grpelem" id="u241-4"><!-- content -->
+       <p>El servidor ha detectado un error.</p>
+      </div>
+      <div class="clearfix grpelem" id="u239-4"><!-- content -->
+       <p>Formulario recibido.</p>
+      </div>
+      <input class="submit-btn NoWrap grpelem" id="u246-17" type="submit" value="" tabindex="4"/><!-- state-based BG images -->
+      <div class="fld-grp clearfix grpelem" id="widgetu235" data-required="false"><!-- none box -->
+       <label class="fld-label actAsDiv clearfix grpelem" id="u238-4" for="widgetu235_input"><!-- content -->
+        <span class="actAsPara"><span class="Sub_formulario" id="u238">Mensaje:</span></span>
+       </label>
+       <span class="fld-textarea actAsDiv clearfix grpelem" id="u237-3"><!-- content --><textarea class="wrapped-input" id="widgetu235_input" name="custom_U235" tabindex="3"></textarea></span>
+      </div>
+     </form>
+    </div>
+    <div class="verticalspacer"></div>
+   </div>
+  </div>
+  <div class="preload_images">
+   <img class="preload" src="images/u246-17-r.png" alt=""/>
+   <img class="preload" src="images/u246-17-m.png" alt=""/>
+   <img class="preload" src="images/u246-17-fs.png" alt=""/>
+  </div>
+  <!-- JS includes -->
+  <script type="text/javascript">
+   if (document.location.protocol != 'https:') document.write('\x3Cscript src="http://musecdn2.businesscatalyst.com/scripts/4.0/jquery-1.8.3.min.js" type="text/javascript">\x3C/script>');
+</script>
+  <script type="text/javascript">
+   window.jQuery || document.write('\x3Cscript src="scripts/jquery-1.8.3.min.js" type="text/javascript">\x3C/script>');
+</script>
+  <script src="scripts/museutils.js?212726984" type="text/javascript"></script>
+  <script src="scripts/jquery.watch.js?4125239756" type="text/javascript"></script>
+  <script src="scripts/jquery.scrolleffects.js?519931148" type="text/javascript"></script>
+  <script src="scripts/webpro.js?114405901" type="text/javascript"></script>
+  <script src="scripts/musewpslideshow.js?3797706250" type="text/javascript"></script>
+  <script src="scripts/jquery.museoverlay.js?254250306" type="text/javascript"></script>
+  <script src="scripts/touchswipe.js?12843993" type="text/javascript"></script>
+  <!-- Other scripts -->
+  <script type="text/javascript">
+   $(document).ready(function() { try {
+(function(){var a={},b=function(a){if(a.match(/^rgb/))return a=a.replace(/\s+/g,"").match(/([\d\,]+)/gi)[0].split(","),(parseInt(a[0])<<16)+(parseInt(a[1])<<8)+parseInt(a[2]);if(a.match(/^\#/))return parseInt(a.substr(1),16);return 0};(function(){$('link[type="text/css"]').each(function(){var b=($(this).attr("href")||"").match(/\/?css\/([\w\-]+\.css)\?(\d+)/);b&&b[1]&&b[2]&&(a[b[1]]=b[2])})})();(function(){$("body").append('<div class="version" style="display:none; width:1px; height:1px;"></div>');
+for(var c=$(".version"),d=0;d<Muse.assets.required.length;){var f=Muse.assets.required[d],g=f.match(/([\w\-\.]+)\.(\w+)$/),k=g&&g[1]?g[1]:null,g=g&&g[2]?g[2]:null;switch(g.toLowerCase()){case "css":k=k.replace(/\W/gi,"_").replace(/^([^a-z])/gi,"_$1");c.addClass(k);var g=b(c.css("color")),h=b(c.css("background-color"));g!=0||h!=0?(Muse.assets.required.splice(d,1),(g!=a[f]>>>24||h!=(a[f]&16777215))&&Muse.assets.outOfDate.push(f)):d++;c.removeClass(k);break;case "js":k.match(/^jquery-[\d\.]+/gi)&&typeof $!=
+"undefined"?Muse.assets.required.splice(d,1):d++;break;default:throw Error("Unsupported file type: "+g);}}c.remove();(Muse.assets.outOfDate.length||Muse.assets.required.length)&&alert("Puede que determinados archivos falten en el servidor o sean incorrectos. Limpie la cache del navegador e inténtelo de nuevo. Si el problema persiste, póngase en contacto con el administrador del sitio web.")})()})();
+/* body */
+Muse.Utils.transformMarkupToFixBrowserProblemsPreInit();/* body */
+Muse.Utils.prepHyperlinks(true);/* body */
+Muse.Utils.resizeHeight()/* resize height */
+$('#u609-4').registerPositionScrollEffect([{"in":[-Infinity,0],"speed":[0,0]},{"in":[0,Infinity],"speed":[0,3]}]);/* scroll effect */
+$('#u609-4').registerOpacityScrollEffect([{"opacity":0,"in":[-Infinity,0],"fade":50},{"opacity":100,"in":[0,0]},{"opacity":0,"in":[0,Infinity],"fade":309}]);/* scroll effect */
+Muse.Utils.initWidget('#slideshowu511', function(elem) { $(elem).data('widget', new WebPro.Widget.ContentSlideShow(elem, {heroFitting:'fillFrameProportionally',autoPlay:true,displayInterval:8000,slideLinkStopsSlideShow:false,transitionStyle:'horizontal',lightboxEnabled_runtime:false,shuffle:false,transitionDuration:700,enableSwipe:true,elastic:'fullWidth',resumeAutoplay:true,resumeAutoplayInterval:3000,playOnce:false})); });/* #slideshowu511 */
+Muse.Utils.initWidget('#widgetu234', function(elem) { new WebPro.Widget.Form(elem, {validationEvent:'submit',errorStateSensitivity:'high',fieldWrapperClass:'fld-grp',formSubmittedClass:'frm-sub-st',formErrorClass:'frm-subm-err-st',formDeliveredClass:'frm-subm-ok-st',notEmptyClass:'non-empty-st',focusClass:'focus-st',invalidClass:'fld-err-st',requiredClass:'fld-err-st',ajaxSubmit:true}); });/* #widgetu234 */
+Muse.Utils.fullPage('#page');/* 100% height page */
+Muse.Utils.showWidgetsWhenReady();/* body */
+Muse.Utils.transformMarkupToFixBrowserProblems();/* body */
+} catch(e) { if (e && 'function' == typeof e.notify) e.notify(); else Muse.Assert.fail('Error calling selector function:' + e); }});
+</script>
+   </body>
+</html>
